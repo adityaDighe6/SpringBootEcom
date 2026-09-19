de400765e3f37cef3992837f4b885869b6e7f130
adityaDighe6 <aditya.mdighe@gmail.com>
2026-09-19 18:16:45 +0530

    Update HelloController for comment test webhook

--- src/main/java/com/springecom/controllers/HelloController.java
+++ src/main/java/com/springecom/controllers/HelloController.java
@@ -2,7 +2,7 @@ package com.springecom.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// this for testing purpose
 @RestController
 public class HelloController {
 	@GetMapping("/home")

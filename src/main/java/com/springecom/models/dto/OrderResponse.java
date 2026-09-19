c9710cf9bdbd25fa2cd1f518907b2abb03b67172
Aditya Mangesh Dighe <aditya.mdighe@gmail.com>
2026-09-19 19:31:03 +0530

    moving to main

---
+++ src/main/java/com/springecom/models/dto/OrderResponse.java
@@ -0,0 +1,14 @@
+package com.springecom.models.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record OrderResponse(
+		String orderId,
+		String customerName,
+		String email,
+		String status,
+		LocalDate orderdate,
+		List<OrderItemResponse> items) {
+
+}
